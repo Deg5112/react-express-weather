83fdb025ca7fd8eb445b016fe7aca762a3a41c80
davidgoodman <dgoodman@tomferry.com>
2021-02-22 14:51:26 -0800

    .env

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,8 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-const apiKey = '94649e58270b1983ed5fd1630892826f';
-
-router.get('/weather-key', async (req, res, next) => {
-  res.json({apiKey}) //
-});
-
 router.get('/weather-data', async ({query: {lat, lon}}, res, next) => {
-  const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+  const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`)
 
   res.json(data);
 });
